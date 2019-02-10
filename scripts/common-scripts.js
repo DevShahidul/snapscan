3f54093e763c26bf0e8b70453ab43730df9ff357
Shahidul <ishahin56@gmail.com>
2019-02-10 21:37:04 +0600

    Added all files

---
+++ scripts/common-scripts.js
@@ -0,0 +1,30 @@
+
+;(function($){
+	$(function(){
+
+        // Begin input common focus and blur for value.
+        $('input:text,input[type="email"],input[type="search"],input[type="tel"],input[type="number"],input:password,textarea').focus(function () {
+            if (this.value == this.defaultValue) {
+                this.value = ''
+            }
+        })
+        $('input:text,input[type="email"],input[type="search"],input[type="tel"],input[type="number"],input:password,textarea').blur(function () {
+            if (!this.value) {
+                this.value = this.defaultValue;
+            }
+        })
+        // Ends input common focus and blur for value.
+		
+        // Phone nav click function 
+        $('#phone-nav').click(function(){
+            $(".nav-wrap").slideToggle();
+        });
+        
+        if($("select.styled-select").length){
+            $('select.styled-select').selectric();
+        }
+       
+		
+	})// End ready function.
+
+})(jQuery)
